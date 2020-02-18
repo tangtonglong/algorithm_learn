4faf04435b638947b094f5e6b41469f9075d3c60
ttl@mbp <532697721@qq.com>
2020-02-18 17:59:42 +0800

    插入

--- algorithm/src/main/java/com/basic/sort/InsertSort.java
+++ algorithm/src/main/java/com/basic/sort/InsertSort.java
@@ -8,13 +8,13 @@ package com.basic.sort;
 public class InsertSort {
 
     public static void main(String[] args){
-        int[] nums = {9527,242045,3733001,134000, 97, 25, 23, 98, 265};
+        int[] nums = {242045,9527,3733001,134000, 97, 25, 23, 98, 265};
 
         int[] numsb = InsertSort.insertSort(nums);
 
-        for (int i = 0; i < numsb.length; i++){
-            System.out.println(numsb[i]);
-        }
+//        for (int i = 0; i < numsb.length; i++){
+//            System.out.println(numsb[i]);
+//        }
     }
 
     /**
@@ -24,6 +24,23 @@ public class InsertSort {
      */
     private static int[] insertSort(int[] nums) {
 
+        for (int i = 0; i < nums.length - 1; i++){
+            int tmpIndex = i + 1;
+            int tmp = nums[tmpIndex];
+            for (int j = i; j >= 0 && j < nums.length; j--){
+                if (tmp < nums[j]){
+                    nums[j+1] = nums[j];
+                    nums[j] = tmp;
+                }else {
+                    System.out.println("sss:" + nums[j]);
+                    break;
+                }
+            }
+
+        }
+        for (int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
 
         return nums;
     }
