00c28604a6cb52a981ec1db54b258d7a15b2096a
tangtonglong <532697721@qq.com>
2020-02-24 15:49:48 +0800

    quicksort修改一下

--- algorithm/src/main/java/com/basic/sort/QuickSort.java
+++ algorithm/src/main/java/com/basic/sort/QuickSort.java
@@ -8,33 +8,82 @@ package com.basic.sort;
 public class QuickSort {
 
 
-    public static void main(String[] args){
-        int[] nums = {9527,242045,3733001,134000, 97, 25, 23, 98, 265};
-        int[] numsB = quickSort(nums);
-        for (int i = 0; i < numsB.length; i++){
+    public static void main(String[] args) {
+//        int[] nums = {9527, 242045, 3733001, 134000, 97, 25, 23, 98, 265};
+        int[] nums = {23, 25, 98};
+        int[] numsB = quickSort(nums, 0, nums.length - 1);
+        for (int i = 0; i < numsB.length; i++) {
             System.out.println(numsB[i]);
         }
+
+
     }
 
-    private static int[] quickSort(int[] nums) {
+    private static int[] quickSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int privot = getPrivot(nums, start, end);
 
+            quickSort(nums, start, privot - 1);
+
+            quickSort(nums, privot + 1, end);
+        }
         return nums;
     }
 
-    public int end2st(int[] nums, int privot, int end){
-        if (privot < end){
-            for (int i = end;i<privot;i--){
-                if (nums[i] < nums[privot]){
+    public static int getPrivot(int[] nums, int start, int end) {
+        int tmpStart = start;
+        int tmpEnd = end;
+        if (tmpStart < tmpEnd && tmpEnd >= start && tmpStart <= end) {
 
+            int privot = tmpStart;
+            while (tmpStart < tmpEnd) {
+//                if (nums[end] >= nums[privot]) {
+//                    end--;
+//                } else {
+//                    int tmp = nums[privot];
+//                    nums[privot] = nums[end];
+//                    nums[end] = tmp;
+//                    privot = end;
+//                }
+//                if (nums[start] <= nums[privot]) {
+//                    start++;
+//                } else {
+//                    int tmp = nums[privot];
+//                    nums[privot] = nums[start];
+//                    nums[start] = tmp;
+//                    privot = start;
+//                }
+                while (tmpEnd >= start && tmpEnd >= tmpStart && nums[tmpEnd] >= nums[privot]) {
+                    tmpEnd--;
+                }
+                if (tmpEnd >= tmpStart) {
+                    int tmp = nums[privot];
+                    nums[privot] = nums[tmpEnd];
+                    nums[tmpEnd] = tmp;
+                    privot = tmpEnd;
+                } else {
+                    return tmpStart;
+                }
+
+                while (tmpStart <= end && tmpStart <= tmpEnd && nums[tmpStart] <= nums[privot]) {
+                    tmpStart++;
+                }
+                if (tmpStart <= tmpEnd) {
+                    int tmp = nums[privot];
+                    nums[privot] = nums[tmpStart];
+                    nums[tmpStart] = tmp;
+                    privot = tmpStart;
+                } else {
+                    return tmpEnd;
                 }
             }
+            if (tmpStart == tmpEnd) {
+                return tmpStart;
+            }
+            return privot;
+        } else {
+            return start;
         }
-
     }
 
-    public int st2end(int[] nums, int start, int privot){
-        if (start < privot){
-
-        }
-    }
 }
