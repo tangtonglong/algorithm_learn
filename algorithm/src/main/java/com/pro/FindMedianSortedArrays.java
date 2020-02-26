b78decf5113dfe8600f66cf5fa5a543bda660572
tangtonglong <532697721@qq.com>
2020-02-26 17:47:27 +0800

    无重复字符的最长子串

--- algorithm/src/main/java/com/pro/FindMedianSortedArrays.java
+++ algorithm/src/main/java/com/pro/FindMedianSortedArrays.java
@@ -1,25 +1,24 @@
 package com.pro;
 
 /**
- *
  * 4. 寻找两个有序数组的中位数
  * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
- *
+ * <p>
  * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- *
+ * <p>
  * 你可以假设 nums1 和 nums2 不会同时为空。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * nums1 = [1, 3]
  * nums2 = [2]
- *
+ * <p>
  * 则中位数是 2.0
  * 示例 2:
- *
+ * <p>
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- *
+ * <p>
  * 则中位数是 (2 + 3)/2 = 2.5
  *
  * @author : ttl
@@ -29,14 +28,13 @@ package com.pro;
 public class FindMedianSortedArrays {
 
 
-    public static void main(String[] args){
-        int[] nums1 = new int[] {1, 3, 8, 13, 29};
-        int[] nums2 = new int[] {2, 9, 12, 56, 90};
-        System.out.println(findMedianSortedArrays(nums1, nums2));
+    public static void main(String[] args) {
+        int[] nums2 = new int[]{1, 3};
+        int[] nums1 = new int[]{2};
+        System.out.println(findMedianSortedArrays2(nums1, nums2));
     }
 
     /**
-     *
      * @param nums1
      * @param nums2
      * @return
@@ -45,32 +43,92 @@ public class FindMedianSortedArrays {
         int m = nums1.length;
         int n = nums2.length;
         if (m > n) { // to ensure m<=n
-            int[] temp = nums1; nums1 = nums2; nums2 = temp;
-            int tmp = m; m = n; n = tmp;
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
         }
         int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
             int j = halfLen - i;
-            if (i < iMax && nums2[j-1] > nums1[i]){
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
                 iMin = i + 1; // i is too small
-            }
-            else if (i > iMin && nums1[i-1] > nums2[j]) {
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
                 iMax = i - 1; // i is too big
-            }
-            else { // i is perfect
+            } else { // i is perfect
                 int maxLeft = 0;
-                if (i == 0) { maxLeft = nums2[j-1]; }
-                else if (j == 0) { maxLeft = nums1[i-1]; }
-                else { maxLeft = Math.max(nums1[i-1], nums2[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
 
                 int minRight = 0;
-                if (i == m) { minRight = nums2[j]; }
-                else if (j == n) { minRight = nums1[i]; }
-                else { minRight = Math.min(nums2[j], nums1[i]); }
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums2[j], nums1[i]);
+                }
 
                 return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int iMin = 0;
+        int iMax = m;
+        int halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && nums1[i] < nums2[j - 1]) {
+                iMin = i + 1;
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1;
+            } else {
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                } else {
+                    int minRight = 0;
+                    if (i == m) {
+                        minRight = nums2[j];
+                    } else if (j == n) {
+                        minRight = nums1[i];
+                    } else {
+                        minRight = Math.min(nums1[i], nums2[j]);
+                    }
+                    return (maxLeft + minRight) / 2.0;
+                }
             }
         }
         return 0.0;
