c87bf6864ae29f3f41158612e0c187a0378a6298
tangtonglong <532697721@qq.com>
2020-02-28 10:54:53 +0800

    无重复字符的最长子串

--- algorithm/src/main/java/com/pro/LongestPalindrome.java
+++ algorithm/src/main/java/com/pro/LongestPalindrome.java
@@ -32,8 +32,8 @@ public class LongestPalindrome {
         watch.start("dp");
         System.out.println(longestPalindrome(a));
         watch.stop();
-        watch.start("dpV2");
-        System.out.println(longestPalindromeV2(a));
+        watch.start("mancher");
+        System.out.println(mancher(a));
         watch.stop();
         System.out.println(watch.prettyPrint());
     }
@@ -102,4 +102,62 @@ public class LongestPalindrome {
     public static String longestPalindromeV2(String s) {
         return s;
     }
+
+
+    /**
+     * mancher算法
+     * @param s
+     * @return
+     */
+    public static String mancher(String s){
+        String T = preProcess(s);
+        int n = T.length();
+        int[] P = new int[n];
+        int C = 0, R = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * C - i;
+            if (R > i) {
+                P[i] = Math.min(R - i, P[i_mirror]);// 防止超出 R
+            } else {
+                P[i] = 0;// 等于 R 的情况
+            }
+
+            // 碰到之前讲的三种情况时候，需要利用中心扩展法
+            while (T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
+                P[i]++;
+            }
+
+            // 判断是否需要更新 R
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
+
+        }
+
+        // 找出 P 的最大值
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+        int start = (centerIndex - maxLen) / 2; //最开始讲的求原字符串下标
+        return s.substring(start, start + maxLen);
+    }
+
+    public static String preProcess(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
+        }
+        String ret = "^";
+        for (int i = 0; i < n; i++)
+            ret += "#" + s.charAt(i);
+        ret += "#$";
+        return ret;
+    }
+
 }
