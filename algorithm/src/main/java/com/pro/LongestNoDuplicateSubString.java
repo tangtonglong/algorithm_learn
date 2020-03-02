c212ed5fdfe02cae2bc39fee832aaeafe16636a9
tangtonglong <532697721@qq.com>
2020-03-02 15:43:08 +0800

    字符串转换整数 (atoi)

--- algorithm/src/main/java/com/pro/LongestNoDuplicateSubString.java
+++ algorithm/src/main/java/com/pro/LongestNoDuplicateSubString.java
@@ -47,31 +47,32 @@ public class LongestNoDuplicateSubString {
     /**
      * 方法二：滑动窗口
      * 算法
-     *
+     * <p>
      * 暴力法非常简单，但它太慢了。那么我们该如何优化它呢？
-     *
+     * <p>
      * 在暴力法中，我们会反复检查一个子字符串是否含有有重复的字符，但这是没有必要的。如果从索引 ii 到 j - 1j−1 之间的子字符串 s_{ij}s
      * ij
      * ​
-     *   已经被检查为没有重复字符。我们只需要检查 s[j]s[j] 对应的字符是否已经存在于子字符串 s_{ij}s
+     * 已经被检查为没有重复字符。我们只需要检查 s[j]s[j] 对应的字符是否已经存在于子字符串 s_{ij}s
      * ij
      * ​
-     *   中。
-     *
+     * 中。
+     * <p>
      * 要检查一个字符是否已经在子字符串中，我们可以检查整个子字符串，这将产生一个复杂度为 O(n^2)O(n
      * 2
-     *  ) 的算法，但我们可以做得更好。
-     *
+     * ) 的算法，但我们可以做得更好。
+     * <p>
      * 通过使用 HashSet 作为滑动窗口，我们可以用 O(1)O(1) 的时间来完成对字符是否在当前的子字符串中的检查。
-     *
+     * <p>
      * 滑动窗口是数组/字符串问题中常用的抽象概念。 窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)[i,j)（左闭，右开）。而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。例如，我们将 [i, j)[i,j) 向右滑动 11 个元素，则它将变为 [i+1, j+1)[i+1,j+1)（左闭，右开）。
-     *
+     * <p>
      * 回到我们的问题，我们使用 HashSet 将字符存储在当前窗口 [i, j)[i,j)（最初 j = ij=i）中。 然后我们向右侧滑动索引 jj，如果它不在 HashSet 中，我们会继续滑动 jj。直到 s[j] 已经存在于 HashSet 中。此时，我们找到的没有重复字符的最长子字符串将会以索引 ii 开头。如果我们对所有的 ii 这样做，就可以得到答案。
-     *
+     * <p>
      * 作者：LeetCode
      * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetcod/
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
      * @param s
      * @return
      */
@@ -103,19 +104,20 @@ public class LongestNoDuplicateSubString {
     /**
      * 方法三：优化的滑动窗口
      * 上述的方法最多需要执行 2n 个步骤。事实上，它可以被进一步优化为仅需要 n 个步骤。我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。 当我们找到重复的字符时，我们可以立即跳过该窗口。
-     *
+     * <p>
      * 也就是说，如果 s[j]s[j] 在 [i, j)[i,j) 范围内有与 j'j
      * ′
-     *   重复的字符，我们不需要逐渐增加 ii 。 我们可以直接跳过 [i，j'][i，j
+     * 重复的字符，我们不需要逐渐增加 ii 。 我们可以直接跳过 [i，j'][i，j
      * ′
-     *  ] 范围内的所有元素，并将 ii 变为 j' + 1j
+     * ] 范围内的所有元素，并将 ii 变为 j' + 1j
      * ′
-     *  +1。
-     *
+     * +1。
+     * <p>
      * 作者：LeetCode
      * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetcod/
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
      * @param s
      * @return
      */
--- algorithm/src/main/java/com/pro/TwoNumsSum.java
+++ algorithm/src/main/java/com/pro/TwoNumsSum.java
@@ -1,20 +1,20 @@
 package com.pro;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
  * 1. 两数之和
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
+ *
  * @author : ttl
  * 2020/2/24 16:05
  * @return
@@ -35,12 +35,12 @@ public class TwoNumsSum {
 
         HashMap<Integer, Integer> tmpMap = new HashMap(nums.length);
         int[] indexs = new int[2];
-        for (int i = 0 ;i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             tmpMap.put(nums[i], i);
         }
-        for (int i = 0;i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int tmp = target - nums[i];
-            if (tmpMap.containsKey(tmp) && tmpMap.get(tmp).compareTo(i) != 0){
+            if (tmpMap.containsKey(tmp) && tmpMap.get(tmp).compareTo(i) != 0) {
                 indexs[0] = i;
                 indexs[1] = tmpMap.get(tmp);
                 return indexs;
@@ -52,10 +52,10 @@ public class TwoNumsSum {
     public static int[] twoSum2(int[] nums, int target) {
 
         HashMap<Integer, Integer> tmpMap = new HashMap(nums.length);
-        for (int i = 0 ;i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int tmp = target - nums[i];
-            if (tmpMap.containsKey(tmp) && tmpMap.get(tmp) != i){
-                return new int[] {tmpMap.get(tmp), i};
+            if (tmpMap.containsKey(tmp) && tmpMap.get(tmp) != i) {
+                return new int[]{tmpMap.get(tmp), i};
             }
             tmpMap.put(nums[i], i);
         }
