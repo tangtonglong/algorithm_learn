f399bacf03862c1ec62ee30068390e369a1fcd70
tangtonglong <532697721@qq.com>
2020-04-20 19:32:17 +0800

    跳跃游戏 II

--- algorithm/src/main/java/com/pro/MinJump.java
+++ algorithm/src/main/java/com/pro/MinJump.java
@@ -2,7 +2,22 @@ package com.pro;
 
 import org.springframework.util.StopWatch;
 
+
 /**
+ * 跳跃游戏 II
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ *
+ * 示例:
+ *
+ * 输入: [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ *
  * @author : ttl
  * 2020/4/17 17:00
  * @return
@@ -42,23 +57,37 @@ public class MinJump {
             }else {
                 int tmpIndex = index + nums[index];
                 int tmpFoots = 0;
+                boolean haveMoreThanZero = false;
                 for (int i = index + 1; i <= tmpIndex && i < dp.length; i++) {
                     if (dp[i] == 1){
+                        haveMoreThanZero = true;
                         tmpFoots = 1;
+                        break;
                     }else if (dp[i] > 1){
-                        tmpFoots =
+                        haveMoreThanZero = true;
+                        if (tmpFoots == 0){
+                            tmpFoots = dp[i];
+                        }else {
+                            tmpFoots = Math.min(tmpFoots, dp[i]);
+                        }
                     }
                 }
-                if (dp[index] > 0){
-                    dp[index]++;
+                if (haveMoreThanZero){
+                    dp[index] = tmpFoots + 1;
                 }
             }
-
             index--;
         }
         for (int i = 0; i < dp.length; i++) {
             System.out.println(dp[i]);
         }
         return dp[0];
+    }
+
+    public static int jumpV2(int[] nums) {
+        if (nums.length == 1){
+            return 0;
+        }
+        return 1;
     }
 }
